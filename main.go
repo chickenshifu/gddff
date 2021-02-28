9e4742f65d5d1a87864925a412f341c3c991f544
chickenshifu_local <tobias@Lingnas-MacBook-Pro.local>
2021-02-28 17:07:06 +0100

    cleared git cache

--- main.go
+++ main.go
@@ -10,13 +10,19 @@ import (
 )
 
 func main() {
+
+	//The program first opens a text file containing all ISINs currently set up as valid instrument in the L&S investment universe (only equities, ex 'International equities'. After this, it retrieves current information about Open, High, Low, Close on yahoo.finance and stores the data into database
+
 	//1. opens current investment universe data
-	retrieveUniverse()
+	investmentUniverse := retrieveUniverse()
+	fmt.Println(investmentUniverse)
 	//2. open yahoo finance and crawl latest data
-	crawlURI()
+	crawlURI("BLA")
 }
 
-func retrieveUniverse() {
+func retrieveUniverse() []string {
+
+	//This function reads in the 'universe.txt' and returns a string array
 
 	var universe []string //array which will be returned and passed into crawlURI()
 
@@ -31,21 +37,22 @@ func retrieveUniverse() {
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
 		isin := scanner.Text()
 		universe = append(universe, isin)
 	}
 
-	fmt.Println(universe)
-
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	return universe
+
 }
 
-func crawlURI() {
-	//This function gets passed ISINs and  calls yahoo finance API and retrieves daily stock data
+func crawlURI(isin string) {
+	//This function gets passed ISIN, calls yahoo finance API and retrieves daily stock data
 
+	fmt.Println(isin)
 	// Variables
 	yahooFinanceURI := "https://query1.finance.yahoo.com/v8/finance/chart/DAI.DE?symbol=DAI.DE?L&interval=1d"
 
@@ -57,5 +64,9 @@ func crawlURI() {
 	}
 
 	fmt.Println(res)
+
+	//https: //www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
+	//1. isins in yahoo symbols umwandeln,
+	//2. yahoo daten ziehen..
 
 }
